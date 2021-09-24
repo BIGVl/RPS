d692547e6bfb49b6e593b8e19bbd954f0cb89d89
BIGVL <vlasq91@gmail.com>
2021-09-24 17:07:24 +0300

    The game can be played for 1 round

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
     let playerQuestion = prompt ("Choose between rock, paper and scissors!");
     let playerSelect = playerQuestion.toLowerCase();
-    let rock = "";
-    let paper ="";
-    let scissors = "";
     let computerSelect = "";
+    let compScore = 0;
+    let playerScore = 0;
+    let addScore = "";
     
    function playersChoice () {
   if (playerSelect === "rock") {
@@ -23,13 +23,13 @@ function computerPlay () {
 
     let computerPlay = Math.floor(Math.random()*3)+1;
 if (computerPlay === 1) {
-   rock = console.log("Computer has choose Rock");
+   console.log("Computer has choose Rock");
 }
 else if (computerPlay === 2) {
-   paper = console.log("Computer has choose Paper");
+  console.log("Computer has choose Paper");
 }
 else if (computerPlay === 3) {
-  scissors =  console.log("Computer has choose Scissors");
+  console.log("Computer has choose Scissors");
 }
 return computerPlay
 }
@@ -41,16 +41,41 @@ function playRound (playerSelect, computerSelect) {
     if (playerSelect === "rock" && computerSelect === 3 || playerSelect === "paper" && computerSelect === 1 || 
     playerSelect === "scissors" && computerSelect === 2  ) {
     
-       let playerWon = console.log("You have won this round!")
+      console.log("You have won this round!")
+      addScore = 1;
     }
     else if (playerSelect === "scissors" && computerSelect === 1 || playerSelect === "rock" && computerSelect === 2 || 
     playerSelect === "paper" && computerSelect === 3) {
-       let computerWon = console.log("You have lost this round!")
+      console.log("You have lost this round!")
+      addScore = 2;
     }
     else if (playerSelect === "scissors" && computerSelect === 3 || playerSelect === "rock" && computerSelect === 1 || 
     playerSelect === "paper" && computerSelect === 2) {
-        let draw = console.log("It's a draw")
+        console.log("It's a draw")
+        addScore = 3 ; 
     }
     
     } 
     console.log(playRound(playerSelect,computerSelect));
+
+    function game () {
+      
+
+        if (addScore == 1) {
+
+          playerScore++;
+            console.log("The score is "+ playerScore + " - " + compScore);
+           
+        }
+        else if (addScore == 2) {
+          compScore++;
+          console.log("The score is "+ playerScore + " - " + compScore);
+          
+        }
+        else if (addScore == 3) {
+          console.log("The score is still "+ playerScore + " - " + compScore)
+        }
+       
+        
+    }
+    console.log(game());
