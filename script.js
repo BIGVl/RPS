67d102989205d0c6f78caefbf5c2f638bfba2b42
BIGVL <vlasq91@gmail.com>
2021-10-18 21:58:49 +0300

    Working on adding DOM, almost finished

--- script.js
+++ script.js
@@ -1,75 +1,86 @@
-let playerQuestion = "";
-let playerSelect = "";
-let computerSelect = "";
 let compScore = 0;
 let playerScore = 0;
+let playerSelect = 0;
 
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const buttons = document.querySelector('.buttons');
+const score = document.querySelector('.score');
+
+
+
+rock.addEventListener('click',e =>{
+ return playerSelect = 1;
+  
+});
+paper.addEventListener('click',e =>{
+ return playerSelect = 2;
+  
+});
+scissors.addEventListener('click', e =>{
+  return playerSelect = 3;
+  
+});
+
+  //random computer choice 
+  function computerPlay () {
+  
+    let result;
+    let random = Math.floor(Math.random() * 3) + 1;
+  
+    if (random === 1) {
+      result = 1;
+    } else if (random === 2) {
+      result = 2;
+    } else if (random === 3) {
+      result = 3;
+    } else {
+      return "error";
+    }
+    return result;
+  };
+ 
+
+
+buttons.addEventListener('click', e => {
+  playRound(playerSelect,computerPlay());
+  
+});
+
+
+
+// actually plays a round and adds a point to the winner or no points if tie
 function playRound (playerSelect, computerSelect) {
-    playerQuestion = prompt ("Choose between rock, paper and scissors!");
-    playerSelect = playerQuestion.toLowerCase();
-  
-    function playersChoice () {
-      if (playerSelect === "rock") {
-          console.log("You have choose Rock")
-      }
-      else if (playerSelect === "paper") {
-        console.log("You have choose Paper")
-      }
-      else if (playerSelect === "scissors") {
-        console.log("You have choose Scissors")
-       }
-    }; 
-    console.log(playersChoice())
-  
-    function computerPlay () {
-  
-      let computerPlay = Math.floor(Math.random()*3)+1;
-  if (computerPlay === 1) {
-     console.log("Computer has choose Rock");
-  }
-  else if (computerPlay === 2) {
-    console.log("Computer has choose Paper");
-  }
-  else if (computerPlay === 3) {
-    console.log("Computer has choose Scissors");
-  }
-  return computerPlay
-  }
-  computerSelect = computerPlay();
-  
-    
-  
-      if (playerSelect === "rock" && computerSelect === 3 || playerSelect === "paper" && computerSelect === 1 || 
-      playerSelect === "scissors" && computerSelect === 2  ) {
+      if (playerSelect === 1 && computerSelect === 3 || playerSelect === 2 && computerSelect === 1 || 
+      playerSelect === 3 && computerSelect === 2  ) {
       
         console.log("You have won this round!")
         playerScore++;
-        return  console.log("The score is "+ playerScore + " - " + compScore);
+        console.log("The score is "+ playerScore + " - " + compScore);
       }
-      else if (playerSelect === "scissors" && computerSelect === 1 || playerSelect === "rock" && computerSelect === 2 || 
-      playerSelect === "paper" && computerSelect === 3) {
+      else if (playerSelect === 3 && computerSelect === 1 || playerSelect === 1 && computerSelect === 2 || 
+      playerSelect === 2 && computerSelect === 3) {
         console.log("You have lost this round!")
         compScore++;
-        return  console.log("The score is "+ playerScore + " - " + compScore);
+        console.log("The score is "+ playerScore + " - " + compScore);
       }
-      else if (playerSelect === "scissors" && computerSelect === 3 || playerSelect === "rock" && computerSelect === 1 || 
-      playerSelect === "paper" && computerSelect === 2) {
+      else if (playerSelect === 3 && computerSelect === 3 || playerSelect === 1 && computerSelect === 1 || 
+      playerSelect === 2 && computerSelect === 2) {
           console.log("It's a draw")
-          return  console.log("The score is "+ playerScore + " - " + compScore);
+          console.log("The score is "+ playerScore + " - " + compScore);
       }
+  if (playerScore===3 || compScore ===3) {
+    game();
+  }
+     
     
     
       };
 
+      // keeps tracking of the score 
 function game () {
- 
 
-
-    playRound(playerSelect,computerSelect);
-    playRound(playerSelect,computerSelect);
-    playRound(playerSelect,computerSelect);
-    playRound(playerSelect,computerSelect);
-    playRound(playerSelect,computerSelect);
 
     if (playerScore < compScore) {
       return console.log("You have lost the game! Try again, I know you can do it!")
@@ -81,7 +92,7 @@ function game () {
           return console.log("It's a tie! What a battle!!")
         }
     }
-    game();
+  
   
 
 
