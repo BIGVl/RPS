d0741e4a04e203bb5e99d52ed82972c8e7a8297b
BIGVL <vlasq91@gmail.com>
2021-10-20 22:16:31 +0300

    Now the score resets, need to add css to make it much more prettier

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let compScore = 0;
 let playerScore = 0;
 let playerSelect = 0;
 
+
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
 const buttons = document.querySelector('.buttons');
 const score = document.querySelector('.score');
+
+
 
 
 
@@ -18,7 +21,7 @@ paper.addEventListener('click',e =>{
  return playerSelect = 2;
   
 });
-scissors.addEventListener('click', e =>{
+scissors.addEventListener('click',e =>{
   return playerSelect = 3;
   
 });
@@ -45,51 +48,49 @@ scissors.addEventListener('click', e =>{
 
 buttons.addEventListener('click', e => {
   playRound(playerSelect,computerPlay());
-  
 });
 
 
 
-// actually plays a round and adds a point to the winner or no points if tie
+// actually plays the rounds and adds a point to the winner or no points if tie
 function playRound (playerSelect, computerSelect) {
       if (playerSelect === 1 && computerSelect === 3 || playerSelect === 2 && computerSelect === 1 || 
       playerSelect === 3 && computerSelect === 2  ) {
       
-        console.log("You have won this round!")
+        score.textContent= ("You have won this round!")
         playerScore++;
-        console.log("The score is "+ playerScore + " - " + compScore);
+        score.textContent= ("The score is "+ playerScore + " - " + compScore);
       }
       else if (playerSelect === 3 && computerSelect === 1 || playerSelect === 1 && computerSelect === 2 || 
       playerSelect === 2 && computerSelect === 3) {
-        console.log("You have lost this round!")
+        score.textContent= ("You have lost this round!")
         compScore++;
-        console.log("The score is "+ playerScore + " - " + compScore);
+        score.textContent= ("The score is "+ playerScore + " - " + compScore);
       }
       else if (playerSelect === 3 && computerSelect === 3 || playerSelect === 1 && computerSelect === 1 || 
       playerSelect === 2 && computerSelect === 2) {
-          console.log("It's a draw")
-          console.log("The score is "+ playerScore + " - " + compScore);
+          score.textContent= ("It's a draw")
+          score.textContent= ("The score is "+ playerScore + " - " + compScore);
       }
-  if (playerScore===3 || compScore ===3) {
-    game();
-  }
-     
-    
-    
-      };
+      if (playerScore===5 || compScore===5) {
+        game();
+        playerScore = 0;
+        compScore = 0;
+      }
+};
 
-      // keeps tracking of the score 
+      // declares the winner
 function game () {
 
 
     if (playerScore < compScore) {
-      return console.log("You have lost the game! Try again, I know you can do it!")
+      return score.textContent= ("You have lost the game! Try again, I know you can do it!")
     }
        else if (playerScore > compScore) {
-         return console.log("You have  won the game! Congratulations!")
+         return score.textContent= ("You have  won the game! Congratulations!")
        }
         else if (playerScore === compScore) {
-          return console.log("It's a tie! What a battle!!")
+          return score.textContent= ("It's a tie! What a battle!!")
         }
     }
   
